e79a7ee3fcf37f9a1512045c659f0fe0bcb7af57
hiscaler <hiscaler@gmail.com>
2015-08-04 09:53:39 +0800

    修正非模型状态下输出报错的问题

--- src/MultiLevelSelecter.php
+++ src/MultiLevelSelecter.php
@@ -2,6 +2,7 @@
 
 namespace yadjet\MultiLevelSelecter;
 
+use yii\base\InvalidConfigException;
 use yii\helpers\Html;
 use yii\widgets\InputWidget;
 
@@ -14,7 +15,6 @@ class MultiLevelSelecter extends InputWidget
 {
 
     public $form;
-    public $options = [];
     public $htmlOptions = [];
     public $jsOptions = [];
     private $_defaultJsOptions = [
@@ -31,17 +31,13 @@ class MultiLevelSelecter extends InputWidget
         parent::init();
         $this->jsOptions = array_merge($this->_defaultJsOptions, $this->jsOptions);
         if (!$this->jsOptions['file']) {
-            throw new \yii\base\InvalidConfigException('Please provide javascript file path.');
+            throw new InvalidConfigException('Please provide javascript file path.');
         }
     }
 
     public function run()
     {
-        if (!isset($this->options['id'])) {
-            $id = $this->getId();
-        } else {
-            $id = $this->options['id'];
-        }
+        $id = $this->options['id'];
         $view = $this->getView();
         MultiLevelSelecterAsset::register($view);
         $tips = $this->jsOptions['hasTip'] ? $this->jsOptions['tipText'] : '';
@@ -54,8 +50,7 @@ EOT;
         if ($this->hasModel()) {
             return $this->form->field($this->model, $this->attribute, ['template' => '{label}{input}' . Html::tag('div', null, ['id' => "{$id}-render"]) . '{hint}{error}'])->hiddenInput($this->htmlOptions);
         } else {
-            list($name, $id) = $this->resolveNameID();
-            return Html::textField($name, $this->value, $this->htmlOptions);
+            return Html::textInput($this->name, $this->value, $this->htmlOptions);
         }
     }
 
